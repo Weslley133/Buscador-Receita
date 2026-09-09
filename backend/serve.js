d7d593570c66280b37678b394b4b5bdff4938959
Weslley Cesar <cesarweslley133@gmai.com>
2026-09-09 19:41:05 -0300

    Ajusta servidor para deploy

--- backend/serve.js
+++ backend/serve.js
@@ -76,9 +76,10 @@ app.post("/api/receita", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
 
                     
